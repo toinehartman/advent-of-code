340435a80c8d08269d5dce3504dd0dd22209ecda
Toine Hartman <tjbhartman@gmail.com>
2020-12-04 19:59:42 +0100

    Solve 4B

--- src/Days/Day04.hs
+++ src/Days/Day04.hs
@@ -15,7 +15,9 @@ import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
 import Data.Char (isSpace)
-import Data.Text (Text)
+import Control.Monad (liftM2)
+import Data.Functor (($>))
+import Data.Text (Text, pack, unpack)
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -27,35 +29,85 @@ blankLine = endOfLine *> endOfLine
 inputParser :: Parser Input
 inputParser = passport `sepBy` blankLine
 
-key :: Parser Key
-key = do
+entry :: Parser Entry
+entry = do
   k <- Data.Attoparsec.Text.take 3
   skip (== ':')
-  v <- Data.Attoparsec.Text.takeTill isSpace
-  return k
+  value k
 
+value :: Text -> Parser Entry
+value "byr" = do BYr <$> decimal
+value "iyr" = do IYr <$> decimal
+value "eyr" = do EYr <$> decimal
+value "hgt" = do
+  d <- decimal
+  unit <- takeTill isSpace
+  return $ Hgt d (unpack unit)
+value "hcl" = do
+  c <- takeTill isSpace
+  return . HCl $ unpack c
+value "ecl" = do
+  c <- takeTill isSpace
+  return . ECl $ unpack c
+value "pid" = do
+  id <- takeTill isSpace
+  return . PID $ unpack id
+value "cid" = do takeTill isSpace $> CID
 passport :: Parser Passport
-passport = key `sepBy` space
+passport = entry `sepBy` space
 
 ------------ TYPES ------------
-type Passport = [Key]
+type Passport = [Entry]
 
-type Key = Text
+data Entry
+  = BYr Int
+  | IYr Int
+  | EYr Int
+  | Hgt Int String
+  | HCl [Char]
+  | ECl String
+  | PID String
+  | CID
+  deriving (Eq, Show)
 
 type Input = [Passport]
 
 type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
-isValid :: Passport -> Bool
-isValid p = length p == 8 || length p == 7 && opt `notElem` p
-  where
-    opt = "cid"
 ------------ PART A ------------
+isValidA :: Passport -> Bool
+isValidA p
+  | l == 7 = CID `notElem` p
+  | otherwise = l == 8
+  where
+    l = length p
 partA :: Input -> OutputA
-partA = length . filter isValid
+partA = length . filter isValidA
 
 ------------ PART B ------------
+isValidB :: Passport -> Bool
+isValidB = all isValidEntry
+
+isValidEntry :: Entry -> Bool
+isValidEntry (BYr y) = y >= 1920 && y <= 2002
+isValidEntry (IYr y) = y >= 2010 && y <= 2020
+isValidEntry (EYr y) = y >= 2020 && y <= 2030
+isValidEntry (Hgt n u)
+  | u == "cm" = n >= 150 && n <= 193
+  | u == "in" = n >= 59 && n <= 76
+isValidEntry (HCl ('#' : c)) = case parseOnly hexadecimal $ pack c of
+  Right (_ :: Integer) -> True
+  Left (_ :: String) -> False
+isValidEntry (ECl s) = s `elem` opts
+  where
+    opts = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+isValidEntry (PID id)
+  | length id == 9 = case parseOnly decimal $ pack id of
+    Right (_ :: Integer) -> True
+    Left (_ :: String) -> False
+isValidEntry CID = True
+isValidEntry _ = False
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = length . filter (liftM2 (&&) isValidA isValidB)
