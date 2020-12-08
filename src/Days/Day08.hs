b3e9afd31e51d3b6ff13f2fe2ee9ac23c82a0524
Toine Hartman <tjbhartman@gmail.com>
2020-12-08 15:24:44 +0100

    Solve 8B

--- src/Days/Day08.hs
+++ src/Days/Day08.hs
@@ -3,7 +3,8 @@ module Days.Day08 (runDay) where
 {- ORMOLU_DISABLE -}
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
+import Data.Either (rights)
+import Data.List (findIndices)
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -38,7 +39,7 @@ type Input = [Instruction]
 
 type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 data Instruction
   = Nop Int
@@ -48,13 +49,18 @@ data Instruction
 
 type Visited = [Int]
 
-eval :: Int -> Visited -> [Instruction] -> Int
+eval :: Int -> Visited -> [Instruction] -> Either Int Int
 eval i s is
-  | visited i s = 0
+  | i >= length is = Right 0 -- End of instruction list reached
+  | visited i s = Left 0 -- Loop
   | otherwise = case is !! i of
     Nop _ -> eval (i + 1) (visit i s) is
-    Acc x -> x + eval (i + 1) (visit i s) is
+    Acc x -> x `add` eval (i + 1) (visit i s) is
     Jmp n -> eval (i + n) (visit i s) is
+  where
+    add :: Int -> Either Int Int -> Either Int Int
+    add x (Left y) = Left $ x + y
+    add x (Right y) = Right $ x + y
 
 visit :: Int -> Visited -> Visited
 visit i vs = xs ++ (y + 1) : ys
@@ -65,10 +71,31 @@ visited :: Int -> Visited -> Bool
 visited i is = is !! i == 1
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA ins = eval 0 s ins
+partA ins = case eval 0 s ins of
+  Left x -> x
+  Right _ -> error "Should not have reached end of instruction list"
   where
     s = replicate (length ins) 0
 
 ------------ PART B ------------
+replace :: [Instruction] -> Int -> [Instruction]
+replace ins i = xs ++ swap y : ys
+  where
+    (xs, y : ys) = splitAt i ins
+    swap :: Instruction -> Instruction
+    swap (Nop x) = Jmp x
+    swap (Jmp x) = Nop x
+
+findSwappables :: [Instruction] -> [Int]
+findSwappables = findIndices swappable
+  where
+    swappable (Nop _) = True
+    swappable (Jmp _) = True
+    swappable _ = False
+
+------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB ins = head . rights . map (eval i s . replace ins) $ findSwappables ins
+  where
+    i = 0
+    s = replicate (length ins) 0
