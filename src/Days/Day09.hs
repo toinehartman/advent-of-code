f90efa118c44571dcb77ce6a6081477c0d1e1a78
Toine Hartman <tjbhartman@gmail.com>
2020-12-09 15:33:57 +0100

    Solve 9A

--- src/Days/Day09.hs
+++ src/Days/Day09.hs
@@ -1,14 +1,7 @@
 module Days.Day09 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
+import Data.Maybe (mapMaybe)
 import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
@@ -21,18 +14,36 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = decimal `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+type OutputA = Int
 
 type OutputB = Void
 
+-- findCounterparts :: Int -> Int -> [Int] -> [Int]
+-- findCounterparts s x = filter (== s - x)
+
+splice :: Int -> Int -> [Int] -> [Int]
+splice x y xs = zs
+  where
+    (_, ys) = splitAt x xs
+    (zs, _) = splitAt (y - x) ys
 ------------ PART A ------------
+-- `checkIfSum2 s xs` returns True if two different elements of xs sum to s
+checkIfSum2 :: (Int, [Int]) -> Maybe Int
+checkIfSum2 (s, xs) =
+  if any ((== s) . sum) $ U.sublistsOfSize 2 xs
+    then Nothing
+    else Just s
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA ins = head $ mapMaybe (checkIfSum2 . (\i -> (ins !! (i + n), splice i (i + n) ins))) [0 .. l - n - 1]
+  where
+    n = 25
+    l = length ins
 
 ------------ PART B ------------
 partB :: Input -> OutputB
