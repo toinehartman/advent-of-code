53ea6dbb0b1419e6c7b8d2f7f2e43ec993ccb2f7
Toine Hartman <tjbhartman@gmail.com>
2020-12-10 17:55:17 +0100

    Solve 10A

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -2,18 +2,11 @@ module Days.Day10 (runDay) where
 
 {- ORMOLU_DISABLE -}
 import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
-
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
+import qualified Data.IntMap as IntMap
+import Data.IntMap.Strict (IntMap, (!))
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,18 +14,26 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = decimal `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+type OutputA = Int
 
 type OutputB = Void
 
+countJumps :: [Int] -> IntMap Int
+countJumps [_] = IntMap.fromAscList [(1, 0), (2, 0), (3, 1)]
+countJumps (x1 : x2 : xs) = IntMap.adjust (+ 1) diff next
+  where
+    diff = x2 - x1
+    next = countJumps (x2 : xs)
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA ins = (counts ! 1) * (counts ! 3)
+  where
+    counts = countJumps . sort $ 0 : ins
 
 ------------ PART B ------------
 partB :: Input -> OutputB
