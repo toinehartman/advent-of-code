c4a75a87349a6a25fa46c8a83b9d0d0e16ce8089
Toine Hartman <tjbhartman@gmail.com>
2020-12-16 19:57:43 +0100

    Baby steps

--- src/Days/Day14.hs
+++ src/Days/Day14.hs
@@ -14,6 +14,8 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
+import Data.Text (unpack)
+import Data.Word (Word64)
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,15 +23,49 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+  msk <- mask
+  endOfLine
+  mms <- mem `sepBy` endOfLine
+  return (Program msk mms)
+
+mask :: Parser Mask
+mask = do
+  string "mask = "
+  Map.fromList <$> chars
+
+chars :: Parser [(Int, Int)]
+chars = do
+  cs <- reverse . unpack <$> Data.Attoparsec.Text.take 36
+  return $ mapMaybe f (zip [0 ..] cs)
+  where
+    f :: (Int, Char) -> Maybe (Int, Int)
+    f (_, 'X') = Nothing
+    f (i, '0') = Just (i, 0)
+    f (i, '1') = Just (i, 1)
+
+mem :: Parser Mem
+mem = do
+  string "mem["
+  i <- decimal
+  string "] = "
+  v :: Word64 <- decimal
+  return (Mem i v)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = Program
 
-type OutputA = Void
+type OutputA = Int
 
 type OutputB = Void
 
+data Program = Program Mask [Mem] deriving (Show)
+
+type Mask = Map Int Int
+
+data Mem = Mem Int Value deriving (Show)
+
+type Value = Word64
 ------------ PART A ------------
 partA :: Input -> OutputA
 partA = error "Not implemented yet!"
